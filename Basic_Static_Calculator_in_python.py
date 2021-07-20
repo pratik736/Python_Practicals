8b75beb73548d86d62a98082e7d1a0e155e535f5
Pratik Y. Patel <62168204+pratik736@users.noreply.github.com>
2021-07-20 17:42:41 +0530

    static calculator

---
+++ Basic_Static_Calculator_in_python.py
@@ -0,0 +1,13 @@
+a = 21
+b = 10
+ans = 0
+ans = a + b
+print ("Addition of",a,"and",b,"is ", ans)
+ans = a - b
+print ("Subtraction of",a,"and",b,"is ", ans)
+ans = a * b
+print ("Multiplication of",a,"and",b,"is ", ans)
+ans = a / b
+print ("Division of",a,"and",b,"is ", ans)
+ans = a % b
+print ("Modulus of",a,"and",b,"is ", ans)
